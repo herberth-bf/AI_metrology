8acfcf118a0fa68408f2811527fc7fa42df8681e
Herberth Birck Fröhlich <herberth-bf@users.noreply.github.com>
2019-10-27 17:19:13 -0300

    FeatureExtractor Class Initialization

---
+++ notebooks/modelling/FeatureExtractors.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 27 17:12:38 2019
+
+# FEATURE EXTRACTORS 
+
+@author: Herberth Frohlich
+"""
+
+class FeatureExtractors:
+    
+    def __init__():
+        pass
+    
+    def stdev_method(self, image):
+        
+        
+        pass
+    
+    
+    def hog_extraction(self, image):
+        
+        pass
+    
+    
+        
+    
